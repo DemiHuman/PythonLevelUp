76d934bfc63f25c38d6e062c6e71958c6e5a11a8
DemiHuman <eropkinp@gmail.com>
2016-12-22 18:03:36 +0300

    PythonLevelUp

---
+++ Циклы/21.py
@@ -0,0 +1,15 @@
+# n = input('Задайте количество строк: ')
+
+m = input('Количество нулей: ')
+
+i = 0
+
+# if n.isdigit() and m.isdigit:
+#     # m = int
+#     for i in range(int(n)):
+
+
+print('0' * m)
+
+# else:
+#     print('Неверный ввод')
